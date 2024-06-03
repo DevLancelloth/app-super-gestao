3d1aff421a8db63f11f556f8739d6f675b806dd0
DevLancelloth <gabriel.silva00@cs.unipe.edu.br>
2024-06-03 17:27:41 -0300

    Adicionadas as propriedades $fillable aos modelos Fornecedor e SiteContato

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
     use HasFactory;
 }
--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
+    protected $table = 'site_contatos';
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
     use HasFactory;
 }
