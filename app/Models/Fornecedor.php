4674a8328dce4696d04b032667304bb2021dc318
DevLancelloth <gabriel.silva00@cs.unipe.edu.br>
2024-06-07 14:18:48 -0300

    Alterações das aulas mais recentes

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
     use HasFactory;
--- database/migrations/2024_05_28_000744_create_fornecedores_table.php
+++ database/migrations/2024_05_28_000744_create_fornecedores_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Attributes\After;
 
 return new class extends Migration
 {
@@ -14,6 +15,7 @@ return new class extends Migration
         Schema::create('fornecedores', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->softDeletes();
             $table->string('nome', 50);
         });
     }
