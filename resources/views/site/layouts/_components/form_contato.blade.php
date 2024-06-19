48fda502f7e25d4c7183c503517cb88b03aea63d
DevLancelloth <gabriel.silva00@cs.unipe.edu.br>
2024-06-19 17:04:46 -0300

    "Updated form_contato.blade.php: added question mark to 'Qual o motivo do contato' option, removed hardcoded motivo_contato options"

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -8,17 +8,13 @@
     <input name="email" value='{{old('email')}}' type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
     <select name="motivo_contato" class="{{ $classe }}">
-        <option value="">Qual o motivo do contato</option>
+        <option value="">Qual o motivo do contato ?</option>
 
         @foreach ($motivo_contatos as $key => $motivo_contato)
 
             <option value="{{$key}}" {{old($motivo_contato) == $key ? 'selected' : ''}} > {{$motivo_contato}}</option>
 
         @endforeach
-
-        <option value="1"{{old('motivo_contatos') == 1 ? 'selected' : '' }}>Dúvida</option>
-        <option value="2"{{old('motivo_contatos') == 2 ? 'selected' : '' }}>Elogio</option>
-        <option value="3"{{old('motivo_contatos') == 3 ? 'selected' : '' }}>Reclamação</option>
     </select>
     <br>
     <textarea name="mensagem" value='{{old('mensagem')}}' class="{{ $classe }}">{{(old('mensagem') != '' ? old('mensagem') : 'Preencha sua mensagem')}}</textarea>
