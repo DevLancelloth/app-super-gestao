6278f9488b21e9d0a194f59b523e92150bafe35e
Gabriel Angelo <gabriel.silva00@cs.unipe.edu.br>
2024-07-23 19:14:14 -0300

    kekw

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(FornecedorSeeder::class);
         $this->call(SiteContatoSeeder::class);
+        $this->call(MotivoContatoSeeder::class);
     }
 }
